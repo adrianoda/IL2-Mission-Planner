098ad422861cd252c5395bf7ced2daf795094e60
ServError <1589596+ServError@users.noreply.github.com>
2023-01-16 05:01:21 +0000

    Add Apollo icon pack.

--- app/js/icons.js
+++ app/js/icons.js
@@ -1,5 +1,31 @@
 module.exports = function(leaflet) {
-
+/* // Prep work for future icon selection cleanup
+    var iconGroups = {
+        "Legacy": {
+            "Red": ["Point", "Marker", "Target", "Bombing Target", "Combat", "Ground Combat", "Takeoff", "Landing", "Base", "Aircraft"],
+            "Blue": ["Point", "Marker", "Target", "Bombing Target", "Combat", "Ground Combat", "Takeoff", "Landing", "Base", "Aircraft"],
+            "Black": ["Point", "Marker", "Target", "Bombing Target", "Combat", "Ground Combat", "Takeoff", "Landing", "Base", "Aircraft"],
+        },
+        "Stock": {
+            "Red": ["Point", "Marker", "Target", "Bombing Target", "Combat", "Ground Combat", "Takeoff", "Landing", "Base", "Aircraft"],
+            "Blue": ["Point", "Marker", "Target", "Bombing Target", "Combat", "Ground Combat", "Takeoff", "Landing", "Base", "Aircraft"],
+            "Spawns": ["Point", "Marker", "Target", "Bombing Target", "Combat", "Ground Combat", "Takeoff", "Landing", "Base", "Aircraft"],
+        },
+        "Apollo": {
+            "Red": ["Point", "Marker", "Target", "Bombing Target", "Combat", "Ground Combat", "Takeoff", "Landing", "Base", "Aircraft"],
+            "Blue": ["Point", "Marker", "Target", "Bombing Target", "Combat", "Ground Combat", "Takeoff", "Landing", "Base", "Aircraft"],
+            "Black": ["Point", "Marker", "Target", "Bombing Target", "Combat", "Ground Combat", "Takeoff", "Landing", "Base", "Aircraft"],
+        },
+        "Random Expert": {
+            "Red": ["Airfield", "Artillery", "Headquarters", "Motorcade", "Active Point", "Point", "Tank", "Warehouse", "Fort"],
+            "Blue": ["Airfield", "Artillery", "Headquarters", "Motorcade", "Active Point", "Point", "Tank", "Warehouse", "Fort"]
+        },
+        "TAW": {
+            "Red": ["Airfield", "Artillery", "Bridge", "City", "Defences", "Depot", "Supply", "Tank", "Train"],
+            "Blue": ["Airfield", "Artillery", "Bridge", "City", "Defences", "Depot", "Supply", "Tank", "Train"]
+        }
+      }
+*/
     const
         URL_PREFIX = 'img/',
         URL_SUFFIX = '.png',
@@ -45,6 +71,9 @@ module.exports = function(leaflet) {
         },
         _isStockIcon: function(type) {
             return type.substring(0, 5) === 'stock';
+        },
+        _isSApolloIcon: function(type) {
+            return type.substring(0, 6) === 'apollo';
         },
         _isCoalitionIcon: function(type) {
             return type.substring(type.length - 5) === 'spawn';
